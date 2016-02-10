7f9f174327793364cb99fc1818678919e271e87b
tcooper <tcooper@localhost>
2016-02-10 16:35:52 -0500

    Fixed the grammatical error

--- BeerSongTSC/src/edu/saintjoe/cs/tcooper/beersongtsc/BeerSongTSC.java
+++ BeerSongTSC/src/edu/saintjoe/cs/tcooper/beersongtsc/BeerSongTSC.java
@@ -7,20 +7,21 @@ public class BeerSongTSC {
 		String word = "bottles";
 		
 		while (beerNum > 0) {
-			if (beerNum == 1) {
-				word = "bottle"; // singular, as in one bottle
-			}
 			
-			System.out.println(beerNum + "" + word + "of beer on the wall");
-			System.out.println(beerNum + "" + word + "of beer.");
+			System.out.println(beerNum + " " + word + " of beer on the wall");
+			System.out.println(beerNum + " " + word + " of beer.");
 			System.out.println("Take one down.");
 			System.out.println("Pass it around.");
 			beerNum = beerNum - 1;
 			
+			if (beerNum == 1) {
+				word = "bottle"; // singular, as in one bottle
+			}
+			
 			if (beerNum > 0) {
-				System.out.println(beerNum + "" + word + "of beer on the wall");
+				System.out.println(beerNum + " " + word + " of beer on the wall");
 			} else {
-				System.out.println("no more bottles of beer on the wall");
+				System.out.println(" no more bottles of beer on the wall");
 			
 			}//end else
 		}//end while loop
