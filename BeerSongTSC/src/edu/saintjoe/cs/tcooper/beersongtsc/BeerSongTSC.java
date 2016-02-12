364ac6dcb2fbc0bdb4315b210bbbedae1022a9be
tcooper <tcooper@localhost>
2016-02-12 16:20:46 -0500

    Added more comments

--- BeerSongTSC/src/edu/saintjoe/cs/tcooper/beersongtsc/BeerSongTSC.java
+++ BeerSongTSC/src/edu/saintjoe/cs/tcooper/beersongtsc/BeerSongTSC.java
@@ -6,19 +6,18 @@ public class BeerSongTSC {
 		int beerNum = 99;
 		String word = "bottles";
 		
-		while (beerNum > 0) {
-			
+		while (beerNum > 0) {//while there's bottles do this
 			System.out.println(beerNum + " " + word + " of beer on the wall");
 			System.out.println(beerNum + " " + word + " of beer.");
 			System.out.println("Take one down.");
 			System.out.println("Pass it around.");
-			beerNum = beerNum - 1;
+			beerNum = beerNum - 1;//subtract 1 from beerNum
 			
 			if (beerNum == 1) {
-				word = "bottle"; // singular, as in one bottle
+				word = "bottle"; //singular, as in one bottle
 			}
 			
-			if (beerNum > 0) {
+			if (beerNum > 0) {//adjust for case of no bottles
 				System.out.println(beerNum + " " + word + " of beer on the wall");
 			} else {
 				System.out.println(" no more bottles of beer on the wall");
